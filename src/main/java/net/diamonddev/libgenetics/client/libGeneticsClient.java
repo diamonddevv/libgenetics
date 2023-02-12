3facba6ef83a53d8aa07c714900271207e7415d1
DiamondDev <fynndiamond@gmail.com>
2023-02-12 15:25:47 +0000

    whoopsies

--- src/main/java/net/diamonddev/libgenetics/client/libGeneticsClient.java
+++ src/main/java/net/diamonddev/libgenetics/client/libGeneticsClient.java
@@ -1,11 +1,9 @@
 package net.diamonddev.libgenetics.client;
 
-import net.diamonddev.libgenetics.common.api.v1.integration.ModIntegrationAPI;
 import net.fabricmc.api.ClientModInitializer;
 
 public class libGeneticsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        ModIntegrationAPI.initializeAll();
     }
 }
--- src/main/java/net/diamonddev/libgenetics/server/libGeneticsServer.java
+++ src/main/java/net/diamonddev/libgenetics/server/libGeneticsServer.java
@@ -1,11 +1,8 @@
 package net.diamonddev.libgenetics.server;
-
-import net.diamonddev.libgenetics.common.api.v1.integration.ModIntegrationAPI;
 import net.fabricmc.api.DedicatedServerModInitializer;
 
 public class libGeneticsServer implements DedicatedServerModInitializer {
     @Override
     public void onInitializeServer() {
-        ModIntegrationAPI.initializeAll();
     }
 }
